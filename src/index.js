c3b765175f607504052d77e44c4f3688cbd23040
wcjameson <williamjameson90@gmail.com>
2022-04-15 17:03:23 -0700

    add github action scripts?

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const storeState = () => {
 const stateControl = storeState();
 
 // This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
+//each object different changeState function?
 
 const changeState = (prop) => {
   return (value) => {
@@ -26,7 +27,7 @@ const changeState = (prop) => {
     })
   }
 }
-
+//store as object of states instead of array of states?
 // We create four functions using our function factory. We could easily create many more.
 
 // const feed = changeState("soil")(1);
@@ -34,6 +35,9 @@ const blueFood = changeState("soil")(5);
 
 // const hydrate = changeState("water")(1);
 // const superWater = changeState("water")(5);
+
+//this works and is cool
+(() => require('./say-hi'))();
 
 $(document).ready(function() {
 
